15384a9fa3c4d228ef74ddefd8667bc664678baf
Mohit Sharma <mohit.gautam2812@gmail.com>
2026-06-11 18:46:53 +0530

    Fix Vercel React routes

--- config/cors.php
+++ config/cors.php
@@ -8,12 +8,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-        'https://your-vercel-app.vercel.app',
-    ],
-
+    'allowed_origins' => ['https://tech-zone-rho.vercel.app'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
